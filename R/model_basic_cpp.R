31a1a9d044da424ad109c3a3447fbdfdf414d528
malicious bunny <juda.jablonska@gmail.com>
2020-07-31 11:56:28 +0200

    model in cpp #2

--- R/model_basic_cpp.R
+++ R/model_basic_cpp.R
@@ -1,18 +1,49 @@
-cppFunction('double modelC(double par1, double par2, NumericVector reward, NumericVector side){
-double q[2];
-            q[0] = q[1] = 0;
-            double p[2];
-            p[0] = p[1] = 0;
-            double nll = 0;
-            int n = side.size();
-            for(int i = 0; i < n; i++){
-            int s = side[i];
-            p[s] = (exp(par2 * q[s])) / (exp(par2 * q[1]) + exp(par2 * q[2]));
-            nll = -log(p[s]) + nll; 
-            double pe = reward[i] - q[s];
-            q[s] = q[s] + (par1 * pe);
-            }
-            return nll;
-            }')
+cppFunction('double modelC(NumericVector par, NumericVector reward, NumericVector side){
+double q[2] = {0,0}, pw, nll, beta, pe;
+short int n = side.size(), s;
+beta = par[1];
+for(int i = 0; i <= n; i++){
+  s = side[i];
+  pw = (exp(beta * q[s])) / (exp(beta * q[0]) + exp(beta * q[1]));
+  if(pw < 0.001){
+    pw = 0.001;
+    }
+  if(pw > 0.999){
+    pw = 0.999;
+    }
+  nll += -log(pw); 
+  pe = reward[i] - q[s];
+  q[s] += (par[0] * pe);
+    }
+return nll;
+  }')
 
-modelC(.1, 1, reward, side)
+model <- function(par, a) {
+  #a = a[with(a, order(start)), ]
+  nll = 0
+    Q = c(0, 0)
+    P <- vector()
+    rewards = a$dooropened
+    sides = ceiling(a$corner / 2)
+    for (i in seq_along(sides)) {
+      r = rewards[i]
+      s = sides[i]
+      P = exp(par[2] * Q) / sum(exp(par[2] * Q))
+      if(P[s] < .001){P[s] = .001}
+      if(P[s] > .999){P[s] = .999}
+      nll = -log(P[s]) + nll
+      pe = r - Q[s]
+      Q[s] = Q[s] + (par[1] * pe)}
+  nll}
+
+modelC(par = c(0.01, 5), 
+       reward = dhero$dooropened, 
+       side = (ceiling(dhero$corner/2)-1))
+
+temp <- bench::mark(
+  model(par = c(0.0001, .5), a = dhero))
+
+temp2 <- bench::mark(
+  modelC(par = c(0.1, .5), 
+         reward = dhero$dooropened, 
+         side = (ceiling(dhero$corner/2)-1)))
