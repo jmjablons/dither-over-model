7d6ccdddb2221c3c286b67b7ae0f5756e4588dad
malicious bunny <juda.jablonska@gmail.com>
2020-09-07 15:28:18 +0200

    try noisy win-stay

---
+++ cpp/model/model_noisywinstay.cpp
@@ -0,0 +1,19 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+double modelC(NumericVector par, NumericVector side, NumericVector win, NumericVector stay){
+  double p[2] = {0.5, 0.5}, nll = 0.0;
+  short int n = side.size(), s;
+  for(int i = 0; i < n; i++){
+    s = side[i];
+    wi = win[i];
+    st = stay[i];
+    if ((wi > 0.1 && st > 0.1) || (wi < 0.1 && st < 0.1)) {
+      p[s] = 1 - (par[0]/2);}
+    if ((wi > 0.1 && st < 0.1) || (wi < 0.1 && st > 0.1)) {
+      p[s] = 0 - (par[0]/2);}
+    p[s] = std::min(p[s], 0.999);
+    p[s] = std::max(p[s], 0.001);
+nll += -log(p[s]);}
+return nll;}
