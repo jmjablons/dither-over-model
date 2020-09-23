0f59421b28c4c84dbc51f7b3a5d960f5283bb1c4
malicious bunny <juda.jablonska@gmail.com>
2020-09-23 15:27:09 +0200

    minor fix

--- cpp/model/model_bdecay.cpp
+++ cpp/model/model_bdecay.cpp
@@ -3,14 +3,13 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 double modelC(NumericVector par, NumericVector reward, NumericVector side, NumericVector interval){
-  double q[2] = {0,0}, pw, nll = 0.0, beta0 = par[1], pe, decay = par[2], t;
-  short int n = side.size(), s, r, vs;
+  double q[2] = {0,0}, pw, nll = 0.0, beta0 = par[1], pe, decay = par[2], t, beta;
+  short int n = side.size(), s, r;
   for(int i = 0; i < n; i++){
     r = reward[i];
     s = side[i];
     t = interval[i];
-    vs = abs(s - 1);
-    beta = exp(0-(t * decay)) * beta0;
+    beta = exp(0-(t * decay)) * (beta0);
     pw = (exp(beta * q[s])) / (exp(beta * q[0]) + exp(beta * q[1]));
     pw = std::min(pw, 0.999);
     pw = std::max(pw, 0.001);
