e352bfdacf54956b1d9dea40a910651b7418830d
malicious bunny <juda.jablonska@gmail.com>
2020-09-30 16:44:08 +0200

    doc noisy win stay #3

--- R/util_eval/util_modelcpp_noisywinstay.R
+++ R/util_eval/util_modelcpp_noisywinstay.R
@@ -1,8 +1,26 @@
-sourceCpp("./cpp/model/model_noisywinstay.cpp")
+parsedata <- function(.tag, .input){
+  temp <- .input[.input$tag == .tag,]
+  temp = temp[with(temp, order(start)),]
+  temp[c("side","win","stay","tag")]}
 
-temp <- dmodel %>%
-  mutate(win = lag(dooropened),
-         stay = ifelse(lag(side) == side, 1, 0)) %>%
-  mutate(win = ifelse(is.na(win), 0, win),
-         stay = ifelse(is.na(stay), 0, stay))
+getoptimal_ <- function(a, list.parameters){
+  out = c(tag = as.numeric(unique(a$tag)), 
+          par = 0, value = Inf, maxPar = 0)
+  for(i in list.parameters){
+    value = modelC(par = i, 
+                   side = a$side, 
+                   win = a$win, 
+                   stay = a$stay)
+    out[4] = i
+    if(value < out[3]){
+      out[2] = i
+      out[3] = value}}
+  out}
 
+run_noisywinstay <- function(a, initial = seq(0.1, 1.999, 0.001)){
+  tags = unique(a$tag)
+  output = list()
+  for (m in tags) {
+    dmouse = parsedata(m, temp)
+    output[[m]] = getoptimal_(dmouse, initial)}
+  do.call(rbind, output)}
---
+++ R/util_eval/util_prepare_date_winstay.R
@@ -0,0 +1,8 @@
+prepare_data_winstay <- function(a = dmodel){
+  a %>% group_by(tag) %>%
+    arrange(start) %>%
+    mutate(win = lag(dooropened),
+           stay = ifelse(lag(side) == side, 1, 0)) %>%
+    ungroup() %>%
+    mutate(win = ifelse(is.na(win), 0, win),
+           stay = ifelse(is.na(stay), 0, stay))}
