2889baab21489a6f6f1ddad4846b37b807abbeb2
jmjablons <juda.jablonska@gmail.com>
2020-10-01 14:03:54 +0200

    plot maps: nll shade x parameter value

--- R/plot/plot_nll_gradient.R
+++ R/plot/plot_nll_gradient.R
@@ -1,22 +1,52 @@
-temp <- expand.grid(alpha = seq(0,1,by = 0.01), beta = seq(0,10, by = 0.1))
-
-dhero <- dmodel %>%
-  filter(tag %in% unique(dmodel$tag)[6])
-
-sourceCpp("cpp/model/model_basic.cpp")
-temp$value <- apply(temp, 1, function(x){
-  modelC(c(x[1],x[2]), dhero$dooropened, side = dhero$side)})
-
+sourceCpp("cpp/model/model_basic.cpp") #modelname
 library(ggplot2)
-library(metR)
-temp %>%
-  filter(beta < 5) %>%
-  ggplot(aes(x = alpha, y = beta, fill = value))+
-  geom_tile()+
-  stat_contour(aes(z = value), 
-               breaks = c(850, 860, 900, 1200, 1700), colour = "white", size = 0.01)+
-  scale_fill_gradient(low = "black", high = "white")+
-  #metR::geom_text_contour(aes(z = value), stroke = 0.3, breaks = c(850, 860, 900, 1200))
-  metR::geom_label_contour(aes(z = value), breaks = c(850, 860, 900, 1200, 1700))
 
-temp$value %>% min()
+plot_parammap_reward_side <- function(.tag, 
+                                      max.beta = 5,
+                                      a = dmodel, modelname = "basic"){
+  temp <- expand.grid(alpha = seq(0,1,by = 0.01), 
+                      beta = seq(0,max.beta, by = 0.1))
+  dmouse <- a %>% filter(tag %in% .tag)
+  temp$value <- apply(temp, 1, function(x){
+    modelC(c(x[1],x[2]), dmouse$dooropened, side = dmouse$side)})
+  temp_tick = plyr::round_any(min(temp$value), 10, ceiling) %>%
+    `+`(c(0, 10, 100))
+  temp_realparam <- remodel[[modelname]] %>%
+    filter(tag %in% .tag) %>%
+    select(grep("par.", names(.)))
+  temp %>%
+    filter(beta < max.beta) %>%
+    ggplot(aes(x = alpha, y = beta, fill = value))+
+    geom_tile()+
+    stat_contour(aes(z = value), 
+                 breaks = temp_tick, colour = "white", size = 0.01)+
+    scale_fill_gradient(low = "black", high = "white")+
+    metR::geom_text_contour(aes(z = value), check_overlap = TRUE, 
+                            size = 2, 
+                            stroke = 0.3, 
+                            breaks = temp_tick)+
+    #metR::geom_label_contour(aes(z = value), breaks = temp_tick)+
+    geom_point(shape = 21, colour = "white", fill = NA,
+               data = filter(temp, value %in% min(value)))+
+    geom_point(aes(x = temp_realparam$par.alpha.pos, 
+                   y = temp_realparam$par.beta), 
+               shape = 8, colour = "white", fill = NA)+
+    scale_x_continuous(limits = c(0,1), 
+                       expand = c(0,0), 
+                       breaks = c(0,1))+
+    scale_y_continuous(limits = c(0, max.beta), 
+                       expand = c(0,0), 
+                       breaks = c(0, max.beta))+
+    theme_classic()+
+    theme(legend.position = "none")+
+    labs(x = latex2exp::TeX("$\\alpha$"), y = latex2exp::TeX("$\\beta$"))}
+
+{tplot <- list()
+for(i in 1:14){
+  m = unique(dmodel$tag)[i]
+  tplot[[i]] = plot_parammap_reward_side(m)}}
+
+ggsave("../inteli-black/result/figure/parammap_basic.pdf", 
+       plot = patchwork::wrap_plots(tplot, ncol = 3), 
+       device = cairo_pdf, 
+       width = 12, height = 18, units = "cm", scale = 1.3)
